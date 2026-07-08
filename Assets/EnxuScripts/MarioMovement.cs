59232c618034c2f939c3aa2f4d94778089f41c96
Leo Enxu <leoenxu@gmail.com>
2026-07-08 12:04:51 +0800

    Mario Movement
    Mario Move, Jump, Climb added

--- Assets/EnxuScripts/MarioMovement.cs
+++ Assets/EnxuScripts/MarioMovement.cs
@@ -15,12 +15,17 @@ public class MarioMovement : MonoBehaviour
     public GameObject groundCheck;
     public GroundCheckScript gcs;
 
+    bool isClimbing = false;
+    BoxCollider2D bCollider;
+    float climbForce = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gcs = FindFirstObjectByType<GroundCheckScript>();
         animator = GetComponent<Animator>();
+        bCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -29,8 +34,10 @@ public class MarioMovement : MonoBehaviour
         //Gets left/right input
         horizontalInput = Input.GetAxis("Horizontal");
 
+        //Flips Sprite depending on where player moves
         FlipSprite();
 
+        //Jump script
         if(Input.GetButtonDown("Jump") && !isJumping && gcs.isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
@@ -38,7 +45,30 @@ public class MarioMovement : MonoBehaviour
             gcs.isGrounded = false;
         }
 
+        //Animation float
         animator.SetFloat("MarioMoving", horizontalInput);
+
+        //If player is on ladder
+        if(isClimbing)
+        {
+            animator.SetBool("MarioClimb", true);
+        }
+        if(!isClimbing)
+        {
+            animator.SetBool("MarioClimb", false);
+        }
+
+        //If player is on ladder and press W/Up
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && isClimbing)
+        {
+            rb.AddForce(transform.up * climbForce);
+        }
+
+        //If player is on ladder and press S/Down
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && isClimbing)
+        {
+            rb.AddForce(transform.up * -climbForce);
+        }
     }
 
     private void FixedUpdate()
@@ -61,5 +91,28 @@ public class MarioMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isJumping = false;
+    }
+
+    //Check if player is on ladder
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.CompareTag("Ladder"))
+        {
+            isClimbing = true;
+            bCollider.isTrigger = true;
+            rb.gravityScale = 0f;
+            isJumping = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.CompareTag("Ladder"))
+        {
+            isClimbing = false;
+            bCollider.isTrigger = false;
+            rb.gravityScale = 0.2f;
+            isJumping = false;
+        }
     }
 }
